0863f42d0a21c56b443f27ec9672bee283d447d1
Byron Hager <byron.hager@jbknowledge.com>
2025-06-03 13:21:23 -0500

    Rename container

--- cerbos/start.ps1
+++ cerbos/start.ps1
@@ -3,5 +3,5 @@ podman run --replace `
   -v "$PSScriptRoot/config:/config" `
   -v "$PSScriptRoot/policies:/policies" `
   -p 3592:3592 -p 3593:3593 `
-  --name claims_cerbos ghcr.io/cerbos/cerbos:0.44.0 `
+  --name test_cerbos ghcr.io/cerbos/cerbos:0.44.0 `
   server --config=/config/.cerbos.yaml
